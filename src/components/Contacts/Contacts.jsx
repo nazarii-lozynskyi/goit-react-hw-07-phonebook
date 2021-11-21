44cc0e06e37ca599d88384a61492f2aae4add198
Nazarii Lozynskyi <nazarko.l.m@gmail.com>
2021-11-21 20:03:28 +0100

    Update Contacts.jsx

--- src/components/Contacts/Contacts.jsx
+++ src/components/Contacts/Contacts.jsx
@@ -1,6 +1,10 @@
-import { useDispatch, useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
 
-import actions from 'redux/phonebook/phonebook-actions';
+import { getFilter } from 'redux/phonebook/phonebook-selectors.js';
+import {
+  useDeleteContactMutation,
+  useFetchContactsQuery,
+} from 'redux/phonebook/phonebook-slice.js';
 
 import { AccountBox, Delete } from '@mui/icons-material';
 import {
@@ -14,53 +18,53 @@ import {
 
 import styles from './Contact.module.css';
 
-const getVisibleContacts = (contacts, filter) => {
-  const normalizedFilter = filter.toLowerCase().trim();
-
-  return contacts.filter(
-    contact =>
-      contact.name.toLowerCase().includes(normalizedFilter) ||
-      contact.number.includes(filter)
-  );
-};
-
 function Contacts() {
-  const contacts = useSelector(({ contacts: { items, filter } }) =>
-    getVisibleContacts(items, filter)
-  );
-  const dispatch = useDispatch();
+  const filter = useSelector(getFilter);
 
+  const getVisibleContacts = (contacts, filter) => {
+    const normalizedFilter = filter.toLowerCase().trim();
+
+    return contacts.filter(
+      contact =>
+        contact.name.toLowerCase().includes(normalizedFilter) ||
+        contact.phone.includes(filter)
+    );
+  };
+  const { data: contacts } = useFetchContactsQuery();
+
+  const [deleteContact] = useDeleteContactMutation();
   return (
     <>
       <List sx={{ bgcolor: 'background.paper' }} className={styles.list}>
-        {contacts.map(({ id, name, number }) => (
-          <ListItem
-            sx={{
-              borderColor: 'primary.main',
-              border: 1,
-              borderRadius: 2,
-            }}
-            className={styles.item}
-            key={id}
-          >
-            <ListItemAvatar>
-              <Avatar sx={{ bgcolor: 'primary.dark' }}>
-                <AccountBox />
-              </Avatar>
-            </ListItemAvatar>
-            <ListItemText primary={name} secondary={number} />
-            <IconButton
-              type="button"
-              edge="end"
-              aria-label="delete"
-              sx={{ marginLeft: '40px' }}
-              onClick={() => dispatch(actions.deleteContact(id))}
-              id={id}
+        {contacts &&
+          getVisibleContacts(contacts, filter)?.map(({ id, name, number }) => (
+            <ListItem
+              sx={{
+                borderColor: 'primary.main',
+                border: 1,
+                borderRadius: 2,
+              }}
+              className={styles.item}
+              key={id}
             >
-              <Delete sx={{ color: 'error.main' }} />
-            </IconButton>
-          </ListItem>
-        ))}
+              <ListItemAvatar>
+                <Avatar sx={{ bgcolor: 'primary.dark' }}>
+                  <AccountBox />
+                </Avatar>
+              </ListItemAvatar>
+              <ListItemText primary={name} secondary={number} />
+              <IconButton
+                type="button"
+                edge="end"
+                aria-label="delete"
+                sx={{ marginLeft: '40px' }}
+                onClick={() => deleteContact(id)}
+                id={id}
+              >
+                <Delete sx={{ color: 'error.main' }} />
+              </IconButton>
+            </ListItem>
+          ))}
       </List>
     </>
   );
