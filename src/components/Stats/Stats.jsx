987fd3d52cfe5a56c52310256db882d14727af70
Nazarii Lozynskyi <nazarko.l.m@gmail.com>
2021-11-21 20:03:37 +0100

    Update Stats.jsx

--- src/components/Stats/Stats.jsx
+++ src/components/Stats/Stats.jsx
@@ -1,9 +1,13 @@
-import { connect } from "react-redux";
+import { connect } from 'react-redux';
 
 function Stats({ total }) {
   return <span>{total}</span>;
 }
 
-const mapStateToProps = (state) => ({ total: state.contacts.items.length });
+const mapStateToProps = state => ({
+  total: 3,
+
+  //total: state.contactsApi.queries.fetchContacts.data.length,
+});
 
 export default connect(mapStateToProps)(Stats);
