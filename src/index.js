531e451e33590e3f95e99c44f64c9298bc16a1ca
Nazarii Lozynskyi <nazarko.l.m@gmail.com>
2021-11-21 20:03:40 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { Provider } from 'react-redux';
 
 //import { PersistGate } from 'redux-persist/integration/react';
 
-import store from './redux/store';
+import { store } from './redux/store';
 
 import './index.css';
 import App from './App';
