0a6d0509249df14ad6ba8f885013e80f325802d3
Nazarii Lozynskyi <nazarko.l.m@gmail.com>
2021-11-22 03:17:03 +0100

    Update Contacts.jsx

--- src/components/Contacts/Contacts.jsx
+++ src/components/Contacts/Contacts.jsx
@@ -1,3 +1,4 @@
+import * as React from 'react';
 import { useSelector } from 'react-redux';
 
 import { getFilter } from 'redux/phonebook/phonebook-selectors.js';
@@ -14,12 +15,18 @@ import {
   ListItem,
   ListItemAvatar,
   ListItemText,
+  Snackbar,
 } from '@mui/material';
+import MuiAlert from '@mui/material/Alert';
 
 import { CSSTransition, TransitionGroup } from 'react-transition-group';
 
 import styles from './Contact.module.css';
 import './Contacts.css';
+
+const Alert = React.forwardRef(function Alert(props, ref) {
+  return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+});
 
 function Contacts() {
   const filter = useSelector(getFilter);
@@ -37,6 +44,21 @@ function Contacts() {
   const { data: contacts } = useFetchContactsQuery();
 
   const [deleteContact] = useDeleteContactMutation();
+
+  // --- Snackbar  /
+  const [open, setOpen] = React.useState(false);
+
+  const handleClick = () => {
+    setOpen(true);
+  };
+
+  const handleClose = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+
+    setOpen(false);
+  };
 
   return (
     <>
@@ -66,7 +88,10 @@ function Contacts() {
                       edge="end"
                       aria-label="delete"
                       sx={{ marginLeft: '40px' }}
-                      onClick={() => deleteContact(id)}
+                      onClick={() => {
+                        deleteContact(id);
+                        handleClick();
+                      }}
                       id={id}
                     >
                       <Delete sx={{ color: 'error.main' }} />
@@ -77,6 +102,12 @@ function Contacts() {
             )}
         </TransitionGroup>
       </List>
+
+      <Snackbar open={open} autoHideDuration={2000} onClose={handleClose}>
+        <Alert onClose={handleClose} severity="error" sx={{ width: '100%' }}>
+          Сontact deleted
+        </Alert>
+      </Snackbar>
     </>
   );
 }
